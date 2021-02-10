cc411aebbdb2d1ee9ea9307deeb1ad07faf4cfd7
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2021-02-09 22:21:53 -0500

    Refactor mute CLI argument

--- AppUtils.py
+++ AppUtils.py
@@ -10,17 +10,6 @@ def get_script_path():
     """Gets the path where the script is running."""
     return os.path.dirname(os.path.realpath(sys.argv[0]))
 
-def str2bool(v):
-    # https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
-    if isinstance(v, bool):
-       return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
-
 def check_positive(value):
     # https://stackoverflow.com/questions/14117415/in-python-using-argparse-allow-only-positive-integers
     ivalue = int(value)
@@ -34,5 +23,5 @@ def parse_args():
     parser = argparse.ArgumentParser(description='PS5 stock helper')
     parser.add_argument('--period', help='How often to check for stock, in seconds. Positive integer', type=check_positive, default=5)
     parser.add_argument('--log', help='Set log level', default='info')
-    parser.add_argument('--mute', help='Toggles audio notification', type=str2bool, default=False)
+    parser.add_argument('--mute', help='Disables audio notification', dest='mute', action='store_true', default=False)
     return vars(parser.parse_args())
